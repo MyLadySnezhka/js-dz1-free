70bb8ce2e32833193987dde204ee8f719087ca13
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-01 16:19:01 +0300

    add second content block

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
-let textout = document.querySelector('.content-block');
+let textlink1 = document.querySelector('.content-block1');
+let textlink2 = document.querySelector('.content-block2');
 
-let textin = document.querySelector('input[name="input-val"]');
-textin.addEventListener('change', (ev) => {
-    let text = ev.target.value;
-    textout.innerHTML = text;
-    console.log(text);
-    
+let textin1 = document.querySelector('input[name="input-val1"]');
+textin1.addEventListener('change', (ev) => {
+    let text1 = ev.target.value;
+    textlink1.innerHTML = text1;
+    // console.log(text1);
 })
+
+let textin2 = document.querySelector('input[name="input-val2"]');
+textin2.addEventListener('change', (ev) => {
+    let text2 = ev.target.value;
+    textlink2.innerHTML = text2;
+    console.log(text2);
+})
+
+
+
 
 
 
